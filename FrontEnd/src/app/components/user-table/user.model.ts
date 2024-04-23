dcf183194e2bf80a61bba53f6698b46052ac4ac1
JotaceCode <josvelsei@gmail.com>
2024-04-23 09:10:17 +0200

    user-profile style settle
    new icons, style added

--- FrontEnd/src/app/components/user-table/user.model.ts
+++ FrontEnd/src/app/components/user-table/user.model.ts
@@ -2,9 +2,11 @@ export interface User{
   id: string | null;
   name:string;
   surname:string;
+  password:string;
   email: string;
   phone: string;
   birthDate: Date;
   pc:string;
   city:string;
+  token:string;
 }
--- FrontEnd/src/app/services/user.service.ts
+++ FrontEnd/src/app/services/user.service.ts
@@ -3,44 +3,52 @@ import { User } from "../components/user-table/user.model";
 export class UserService{
   private userList: User[] = [
     {
-      id: "1",
-      birthDate: new Date("1990-05-15"),
-      name: "Luis",
-      surname: "Martínez",
-      email: "luis.martinez@example.com",
-      phone: "600 123 456",
-      pc: "28001",
-      city: "Madrid"
+      "id": "1",
+      "name": "Luis",
+      "surname": "Martínez",
+      "password": "contraseña1",  // Ejemplo de contraseña
+      "email": "luis.martinez@example.com",
+      "phone": "600 123 456",
+      "birthDate":  new Date("09-01-1992"),
+      "pc": "28001",
+      "city": "Madrid",
+      "token": "token1"  // Ejemplo de token
     },
     {
-      id: "2",
-      birthDate: new Date("1985-08-20"),
-      name: "Ana",
-      surname: "González",
-      email: "ana.gonzalez@example.com",
-      phone: "600 987 654",
-      pc: "08001",
-      city: "Barcelona"
+      "id": "2",
+      "name": "Ana",
+      "surname": "González",
+      "password": "contraseña2",  // Ejemplo de contraseña
+      "email": "ana.gonzalez@example.com",
+      "phone": "600 987 654",
+      "birthDate":  new Date("06-02-1998"),
+      "pc": "08001",
+      "city": "Barcelona",
+      "token": "token2"  // Ejemplo de token
     },
     {
-      id: "3",
-      birthDate: new Date("1993-03-10"),
-      name: "Carlos",
-      surname: "Pérez",
-      email: "carlos.perez@example.com",
-      phone: "600 111 222",
-      pc: "41001",
-      city: "Sevilla"
+      "id": "3",
+      "name": "Carlos",
+      "surname": "Pérez",
+      "password": "contraseña3",  // Ejemplo de contraseña
+      "email": "carlos.perez@example.com",
+      "phone": "600 111 222",
+      "birthDate": new Date("06-02-1992"),
+      "pc": "41001",
+      "city": "Sevilla",
+      "token": "token3"  // Ejemplo de token
     },
     {
-      id: "4",
-      birthDate: new Date("1988-11-25"),
-      name: "Elena",
-      surname: "Rodríguez",
-      email: "elena.rodriguez@example.com",
-      phone: "600 333 444",
-      pc: "46001",
-      city: "Valencia"
+      "id": "4",
+      "name": "Elena",
+      "surname": "Rodríguez",
+      "password": "contraseña4",  // Ejemplo de contraseña
+      "email": "elena.rodriguez@example.com",
+      "phone": "600 333 444",
+      "birthDate":  new Date("06-02-1995"),
+      "pc": "46001",
+      "city": "Valencia",
+      "token": "token4"  // Ejemplo de token
     }
   ];
 
