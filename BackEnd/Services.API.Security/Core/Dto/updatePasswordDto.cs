9175b146a8d53d2299dc897afd34706723802453
JotaceCode <josvelsei@gmail.com>
2024-05-03 12:13:59 +0200

    RESET BRANCH

--- BackEnd/Services.API.Security/Core/Dto/updatePasswordDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Services.API.Security.Core.Dto
-{
-    public class UpdatePasswordDto
-    {
-
-        public string oldPassword { get; set; }
-        public string newPassword { get; set; }
-    }
-}
--- FrontEnd/src/main.ts
+++
@@ -1,6 +0,0 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { RootComponent } from './app/root.component';
-
-bootstrapApplication(RootComponent, appConfig)
-  .catch((err) => console.error(err));
