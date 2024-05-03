b40860e008fc130e3db203c310813f3b960f8348
Mendozaa <orugamendoza@gmail.com>
2024-05-03 12:11:02 +0200

    reset project

--- BackEnd/Services.API.Security/Core/Dto/FilterParameters.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Services.API.Security.Core.Dto
-{
-    public class FilterParameters
-    {
-        public required string Property { get; set; }
-        public required string Value { get; set; }
-    }
-}
--- BackEnd/Services.API.Security/Core/Dto/ResetPasswordDto.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Services.API.Security.Core.Dto
-{
-    public class ResetPasswordDto
-    {
-        public string newPassword { get; set; }
-    }
-}
